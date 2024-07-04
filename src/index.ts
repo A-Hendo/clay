bfb6d5c7845cc6bf0c44caeacae4007c41024dd0
Adam Henderson <adamhenderson90@gmail.com>
2024-07-04 21:44:59 +0100

    Fix package not being installed globally

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { Command } from "commander";
 import figlet from "figlet";
 import { SvelteKitCommands } from "./commands/svelte/index.js";
@@ -18,7 +20,7 @@ const program = new Command();
 program
     .name("crejs")
     .description("A CLI for scaffolding applications")
-    .version("0.1.1")
+    .version("0.1.3")
 
 SvelteKitCommands(program);
 
